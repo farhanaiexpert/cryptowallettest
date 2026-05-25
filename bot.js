bbcb817e137d4f73d5b83c4d5617e3bfa98ac9a2
farhanaiexpert <farhan.aiexpert@gmail.com>
2026-05-26 00:36:56 +0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,4 +1,4 @@
-require('dotenv').config({ path: require('path').join(__dirname, '..', '.env') });
+require('dotenv').config({ path: require('path').join(__dirname, '.env') });
 const TelegramBot = require('node-telegram-bot-api');
 const fs = require('fs');
 const path = require('path');
